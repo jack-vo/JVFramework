2c6454d9dd5d5b90b29b53e0938a50e488e56fa2
Jack Vo <jack.vo@learnosity.com>
2018-04-11 21:26:46 +1000

    WIP

---
+++ Assets/JV/Scripts/Weapons/HandleMeleeWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JV {
+    public class HandleMeleeWeapon : BaseBehavior {
+
+		public override void UpdateBehavior () {
+            if (controller.anim.GetBool("Melee")) {
+                controller.anim.SetBool ("Melee", false);
+            }
+
+            if (Input.GetButtonDown("melee")) {
+                controller.anim.SetBool ("Melee", true);
+            }
+		}
+	}
+
+}
