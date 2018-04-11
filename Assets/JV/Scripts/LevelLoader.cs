d94550177094d2f45c456f73104c89fa7c4da826
Jack Vo <chuongxip@yahoo.com>
2018-04-11 22:56:43 +1000

    WIP

---
+++ Assets/JV/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JV {
+    public class LevelLoader : MonoBehaviour {
+
+        bool playerInZone;
+
+        public string levelToLoad;
+
+        // Update is called once per frame
+        void Update () {
+            if (playerInZone && Input.GetButtonDown("up")) {
+                SceneManager.LoadSceneAsync (levelToLoad);
+            }
+        }
+
+        void OnTriggerEnter2D (Collider2D other) {
+            if (other.name == "Player") {
+                playerInZone = true;
+            }
+        }
+
+        void OnTriggerExit2D (Collider2D other) {
+            if (other.name == "Player") {
+                playerInZone = false;
+            }
+        }
+    }
+}
+
